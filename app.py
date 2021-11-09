50b9d05c93390769c33efedc3808eb46fbcd4b21
yijieee44 <54114112+yijieee44@users.noreply.github.com>
2021-11-09 22:37:04 +0800

    add graph

--- app.py
+++ app.py
@@ -1,60 +1,56 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 from werkzeug.utils import secure_filename
 import pandas as pd
+import warnings
 
-from src.model.pycaret_compare import reg_compare, cla_compare
+from src.model.pycaret_compare import reg_compare, cla_compare, reg_create_graph, cla_create_graph
 
 app = Flask(__name__)
 type_list = ["Regression", "Classification"]
-result = ""
+error_message = ""
+warnings.filterwarnings("ignore")
 
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('home.html', type_list=type_list, result=result, 
-    selected_type=type_list[0], selected_des="", target_name="")
+    return render_template('home.html', type_list=type_list, error_message=error_message, 
+    selected_type=type_list[0], selected_des="", target_name="", graph_url="./static/images/graph.png")
 
 
 @app.route('/', methods=['POST'])
 def submit():
     selected_type = request.form.get('selected_type')
     target_name = request.form.get('target_name')
+    error_message = ""
 
     if 'file' not in request.files:
-        result = "## Error: No file uploaded"
-        return render_template('home.html', type_list=type_list, result=result, selected_type=selected_type, selected_des="", target_name=target_name)
+        error_message = "## Error: No file uploaded"
+        return render_template('home.html', type_list=type_list, error_message=error_message, selected_type=selected_type, selected_des="", target_name=target_name)
 
     file = request.files['file']
     
     if file.filename == '':
-        result = "## Error: No file selected"
+        error_message = "## Error: No file selected"
         selected_des = ""
 
     if file:
-        # generate result in string form here
+        # generate error_message in string form here
         selected_des = file.filename
         selected_des = selected_des + " -- " + selected_type
     
         df = pd.read_csv(file.stream)
-        df = clean(df)
         if selected_type == "Regression":
             best_model, result_df = reg_compare(df, target_name)
+            reg_create_graph(result_df)
         elif selected_type == "Classification":
             best_model, result_df = cla_compare(df, target_name)
-        result = result_df.to_string()
-    
-    return render_template('home.html', type_list=type_list, result=result, selected_type=selected_type, selected_des=selected_des, target_name=target_name)
+            cla_create_graph(result_df)
+        
+        # result_df.to_csv("temp.csv", sep=",", index=False)
 
-def clean(df):
-    df = df[~df[' Income '].isnull()]
-    df[' Income '] = df[' Income '].apply(lambda x: x[1:].replace(',', '').split('.')[0])
-    df[' Income '] = df[' Income '].astype('int32')
-
-    df['Education'] = df['Education'].astype('category')
-    df['Marital_status'] = df['Marital_Status'].astype('category')
-    df['Country'] = df['Country'].astype('category')
-
-    df.drop(['ID', 'Dt_Customer'], inplace=True, axis=1)
-    return df
+    return render_template('home.html', type_list=type_list, error_message=error_message, 
+    selected_type=selected_type, selected_des=selected_des, target_name=target_name,
+    result_header=result_df.columns.values, tables=[result_df.to_html(classes='data')],
+    graph_url="./static/images/graph.png")
 
 
 if __name__ == "__main__":
--- src/model/pycaret_compare.py
+++ src/model/pycaret_compare.py
@@ -1,6 +1,9 @@
 from pycaret import regression as reg
 from pycaret import classification as cla
+import matplotlib.pyplot as plt
+import warnings
 
+warnings.filterwarnings("ignore")
 
 def reg_compare(df, target):
     reg.setup(data = df, target = target, session_id=123, silent=True, verbose=False)
@@ -14,3 +17,59 @@ def cla_compare(df, target):
     best_model = cla.compare_models(verbose=False)
     result_df = cla.pull()
     return best_model, result_df
+
+
+def reg_create_graph(df):
+    columns = df.columns.values
+    metrics = columns[1:len(columns)-1]
+
+    fig = plt.figure(figsize=(40,20))
+    fig.tight_layout()
+
+    models = df['Model'].tolist()
+    
+    i = 0
+    while i != len(metrics):
+        axis = fig.add_subplot(4, 2, i+1)
+        
+        values = df[metrics[i]].tolist()
+        values_sort, models_sort = zip(*sorted(zip(values, models)))
+
+        values_sort = list(values_sort)
+        models_sort = list(models_sort)
+        models_sort.reverse()
+        values_sort.reverse()
+
+        axis.barh(models_sort, values_sort)
+        axis.set_title(metrics[i])
+
+        i+=1
+
+    fig.savefig('./static/images/graph.png', bbox_inches='tight')
+
+
+def cla_create_graph(df):
+    columns = df.columns.values
+    metrics = columns[1:len(columns)-1]
+
+    fig = plt.figure(figsize=(40,20))
+    fig.tight_layout()
+
+    models = df['Model'].tolist()
+    
+    i = 0
+    while i != len(metrics):
+        axis = fig.add_subplot(4, 2, i+1)
+        
+        values = df[metrics[i]].tolist()
+        values_sort, models_sort = zip(*sorted(zip(values, models)))
+
+        values_sort = list(values_sort)
+        models_sort = list(models_sort)
+
+        axis.barh(models_sort, values_sort)
+        axis.set_title(metrics[i])
+
+        i+=1
+
+    fig.savefig('./static/images/graph.png', bbox_inches='tight')
